dc0e655dbf39a0613dbb5de207fdebcc2ff7dec8
strimnal32246 <susan.trimnal@icdccollege.edu>
2020-01-06 13:40:55 -0500

    added server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+//import routes
+const authRouter = require("./auth/auth-router");
+const usersRouter = require("./users/users-router");
+const postsRouter = require("./posts/posts-router");
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
+server.use("/api/posts", postsRouter);
+
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "Backend up and running" });
+});
+
+module.exports = server;
