37105fd3076017e6780f4da19f00ef089b22dbc3
strimnal32246 <susan.trimnal@icdccollege.edu>
2020-01-07 20:08:53 -0500

    made changes to port

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const server = require("./api/server");
+const server = require("./server");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8550;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
